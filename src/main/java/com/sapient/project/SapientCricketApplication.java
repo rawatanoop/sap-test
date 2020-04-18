e82f3ebf1fd17418778d241c205a257f55e3dc8e
Anoop Rawat <wb-anooprawa595037@antfin.com>
2020-04-18 11:07:56 +0530

    Initial commit

---
+++ src/main/java/com/sapient/project/SapientCricketApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapientCricketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapientCricketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapient/project/SapientCricketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapientCricketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
